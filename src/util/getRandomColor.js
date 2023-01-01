3c70af6255ccd298cf89f5a7c175b5a596d688af
LongHaHoang <longhahoang1992@gmail.com>
2023-01-01 03:29:21 +0100

    add hex color

---
+++ src/util/getRandomColor.js
@@ -0,0 +1,12 @@
+export default function getRandomColor() {
+  const getRgb = () => Math.floor(Math.random() * 256);
+
+  function hexValue() {
+    const hex = getRgb().toString(16);
+    return hex.length === 1 ? "0" + hex : hex;
+  }
+
+  const colorHex = `#${hexValue()}${hexValue()}${hexValue()}`;
+
+  return console.log(colorHex);
+}
